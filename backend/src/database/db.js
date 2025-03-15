8213124d702d3fa2a3058d84199ab24d1146b59b
Onkar Karale <onkarkarale4@gmail.com>
2025-03-16 01:14:29 +0530

    Updated previous code

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -9,8 +9,8 @@ const connectDb = async () => {
   try {
     await mongoose.connect(DB_URL)
     console.log('Successfully connected to database')
-  } catch (error) {
-    console.log('Error connecting to database', error)
+  } catch (err) {
+    console.log('Error connecting to database', err)
   }
 }
 
